8eb0c849aac91ca39495a16d5994ce9d0f8931b8
thi-cr <th.cravatte@outlook.com>
2021-05-12 14:34:13 +0200

    oops

--- models/dao/AnimalDAO.php
+++ models/dao/AnimalDAO.php
@@ -3,5 +3,9 @@
 
 class AnimalDAO extends AbstractDAO
 {
-
+    public function __construct()
+    {
+        //appelle le constructeur du parent (AbstractDAO)
+        parent::__construct('');
+    }
 }
