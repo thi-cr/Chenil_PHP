d1c2a91c69293f446ca0e02d262f8dec62bfa3e2
thi-cr <th.cravatte@outlook.com>
2021-05-12 12:01:03 +0200

    abstractDAO

--- models/dao/AbstractDAO.php
+++ models/dao/AbstractDAO.php
@@ -6,7 +6,7 @@ abstract class AbstractDAO {
 
     public function __construct ($table) {
         $this->table = $table;
-        $this->connection = new PDO('mysql:host=localhost;dbname=poke', 'root', '');
+        $this->connection = new PDO('mysql:host=localhost;dbname=projet_chenil', 'root', '');
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
