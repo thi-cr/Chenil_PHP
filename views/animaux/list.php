c72c3d7ca63fff48ed27ba188648d2956f1af73b
thi-cr <th.cravatte@outlook.com>
2021-05-13 11:33:52 +0200

    list

--- views/animaux/list.php
+++ views/animaux/list.php
@@ -1,4 +1,20 @@
-<?php
-echo "bruh";
-var_dump($animaux);
-?>
+<?php if (!empty($animaux)): ?>
+    <section id="animal-list">
+        <table>
+            <?php foreach ($animaux as $animal): ?>
+                <tr>
+                    <td><?= $animal->__get('id'); ?></td>
+                    <td><?= $animal->__get('nom'); ?></td>
+                    <td><?= $animal->__get('sexe'); ?></td>
+                    <td><?= $animal->__get('sterilise'); ?></td>
+                    <td><?= $animal->__get('date_naissance'); ?></td>
+                    <td><?= $animal->__get('numero_puce'); ?></td>
+                    <td><?= $animal->race->__get('nom'); ?></td>
+                    <td><?= $animal->personne->__get('nom'); ?></td>
+                    <td><?= $animal->personne->__get('prenom'); ?></td>
+                    <td><a href="/animaux/show/<?= $animal->id; ?>">VOIR</a></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </section>
+<?php endif; ?>
