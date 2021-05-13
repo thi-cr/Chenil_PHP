140b90f58ce0443bf946d4ccddbe101516e90288
thi-cr <th.cravatte@outlook.com>
2021-05-13 11:56:20 +0200

    list finito

--- views/animaux/list.php
+++ views/animaux/list.php
@@ -3,15 +3,16 @@
         <table>
             <?php foreach ($animaux as $animal): ?>
                 <tr>
+                    <?php var_dump($animal) ?>
                     <td><?= $animal->__get('id'); ?></td>
                     <td><?= $animal->__get('nom'); ?></td>
                     <td><?= $animal->__get('sexe'); ?></td>
                     <td><?= $animal->__get('sterilise'); ?></td>
                     <td><?= $animal->__get('date_naissance'); ?></td>
                     <td><?= $animal->__get('numero_puce'); ?></td>
-                    <td><?= $animal->race->__get('nom'); ?></td>
-                    <td><?= $animal->personne->__get('nom'); ?></td>
-                    <td><?= $animal->personne->__get('prenom'); ?></td>
+                    <td><?= $animal->race_id->__get('nom'); ?></td>
+                    <td><?= $animal->proprietaire_id->__get('nom'); ?></td>
+                    <td><?= $animal->proprietaire_id->__get('prenom'); ?></td>
                     <td><a href="/animaux/show/<?= $animal->id; ?>">VOIR</a></td>
                 </tr>
             <?php endforeach; ?>
