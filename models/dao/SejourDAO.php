3fdc3ce0f91e5dcf024dd3a3614ae0e37352f01d
thi-cr <th.cravatte@outlook.com>
2021-05-14 12:07:10 +0200

    hahahahahahahahahahahhahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaahhahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha

--- models/dao/SejourDAO.php
+++ models/dao/SejourDAO.php
@@ -33,4 +33,58 @@ class SejourDAO extends AbstractDAO
             $this->animal($result['animal_id'])
         );
     }
+
+    function delete($data)
+    {
+        if (empty($data['id'])) {
+            return false;
+        }
+
+        try {
+            $statement = $this->connection->prepare("DELETE FROM {$this->table} WHERE id = ?");
+            $statement->execute([
+                $data['id']
+            ]);
+        } catch (PDOException $e) {
+            print $e->getMessage();
+        }
+    }
+
+    public function update($id, $data)
+    {
+        try {
+            $statement = $this->connection->prepare("UPDATE {$this->table} SET date_debut = ?, date_fin = ?, animal_id = ? WHERE id = ?");
+            $statement->execute(
+                [
+                    htmlspecialchars($data['date_debut']),
+                    htmlspecialchars($data['date_fin']),
+                    htmlspecialchars($data['animal']),
+                    htmlspecialchars($data['id'])
+                ]
+            );
+        } catch (PDOException $e) {
+            print $e->getMessage();
+        }
+    }
+
+    function store($id, $data)
+    {
+        if (empty($data['date_debut']) || empty($data['date_fin']) || empty($data['animal'])) {
+            return false;
+        }
+
+        try {
+            $statement = $this->connection->prepare(
+                "INSERT INTO {$this->table} (date_debut, date_fin, animal_id ) VALUES (?, ?, ?)"
+            );
+            $statement->execute([
+                htmlspecialchars($data['date_debut']),
+                htmlspecialchars($data['date_fin']),
+                htmlspecialchars($data['animal']),
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            print $e->getMessage();
+        }
+    }
 }
