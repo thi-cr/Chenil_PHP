aa99e106fc62f1a085c5a3289f36da8624565bda
thi-cr <th.cravatte@outlook.com>
2021-05-14 13:50:05 +0200

    hahahahahahahahahahahhahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaahhahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha

--- views/especes/ajouter.php
+++ views/especes/ajouter.php
@@ -1,7 +1,7 @@
 <section>
     <form action="/especes/store" method="post">
         <br><label>Nom</label>
-        <input type="text" name="nom">
+        <input type="text" name="nom" required>
 
         <input type="hidden" name="id" value="<?= $id = 0; ?>">
         <input type="submit" value="AJOUTER" name="addespece">
